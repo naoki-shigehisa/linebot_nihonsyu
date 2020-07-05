186c92c44b5f2f2849c6bdca6f70b5f056afec27
naoki shigehisa <43877096+naoki-shigehisa@users.noreply.github.com>
2020-07-05 22:46:08 +0900

    Update call_from_php_gensim.py

--- call_from_php_gensim.py
+++ call_from_php_gensim.py
@@ -2,15 +2,14 @@
 import re
 from gensim.models import word2vec
 
-def word2vec_list(txt,list_,model):
+#類似度の高い日本酒を探す関数
+def recommend_sake(txt,sake_list,model):
     results = model.wv.most_similar(positive=txt,topn=209127)
     results_sake = []
     for i in range(len(results)):
-        if results[i][0] in list_:
-            results_sake.append(results[i])
-            if len(results_sake) > 9:
-                return results_sake
-    return results_sake
+        if results[i][0] in sake_list:
+            return("あなたにオススメの日本酒は" + str(results[i][0]) + "です！")
+    return "単語が登録されていませんでした"
 
 #モデル読み込み
 model = word2vec.Word2Vec.load("./data/sake.model")
@@ -26,10 +25,6 @@ if __name__=='__main__':
     text = sys.argv[1]
     text = re.split("[ 　,、]",text)
 
-    try:
-        result = word2vec_list(text,sake,model)[0][0]
-        out = "あなたにオススメの日本酒は"+result+"です！"
-    except:
-        out = "単語が登録されていませんでした"   
+    message = recommend_sake(text,sake,model)
 
-    print (out)
+    print (message)
